e29382812e37f4f08dc6025617fb77fb4404a0b9
renderVis <rendervis333@gmail.com>
2020-08-18 10:03:59 +0300

    app scaffolding with base components and path with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-function App() {
+import StreamCreate from "./components/streams/stream_create";
+import StreamDelete from "./components/streams/stream_delete";
+import StreamEdit from "./components/streams/stream_edit";
+import StreamList from "./components/streams/stream_list";
+import StreamShow from "./components/streams/stream_show";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={StreamList} />
+          <Route exact path="/streams/new" component={StreamCreate} />
+          <Route exact path="/streams/edit" component={StreamEdit} />
+          <Route exact path="/streams/delete" component={StreamDelete} />
+          <Route exact path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
+
+// git commit -m "video streamer app. base react project with npx create"
