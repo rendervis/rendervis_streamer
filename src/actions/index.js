aacc2b323e58a6831a14305d2348a9527d38185a
renderVis <rendervis333@gmail.com>
2020-08-24 09:57:29 +0300

    installed axios and redux-thunk. axios post to create record from input data.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,14 @@
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STEAMS,
+  FETCH_STEAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
+
+import streams from "../apis/streams";
 
 export const signIn = (userId) => {
   return {
@@ -10,5 +20,15 @@ export const signIn = (userId) => {
 export const signOut = () => {
   return {
     type: SIGN_OUT,
+  };
+};
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    const response = await streams.post("/streams", formValues);
+    dispatch({
+      type: CREATE_STREAM,
+      payload: response.data,
+    });
   };
 };
